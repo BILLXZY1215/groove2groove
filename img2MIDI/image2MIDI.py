9b9fd02c54ad854a946abc7a1416984e7d6b0972
Zeyu Xiong <scyzx2@gmail.com>
2021-11-09 22:42:52 +0800

    update image2MIDI

--- img2MIDI/image2MIDI.py
+++ img2MIDI/image2MIDI.py
@@ -1,6 +1,13 @@
 import cv2
+from PIL import Image
+import numpy as np
+import pretty_midi
 
-img = cv2.imread('Amily.jpeg')
-cv2.imshow('output', img)
-cv2.waitKey(2000)
-cv2.destroyAllWindows()
+# img = cv2.imread('Amily.jpeg')
+# cv2.imshow('output', img)
+# cv2.waitKey(2000)
+# cv2.destroyAllWindows()
+
+img = np.array(Image.open('Amily.jpeg'))  # RGB Matrix
+img = cv2.resize(img, (224, 224))
+print(img)
