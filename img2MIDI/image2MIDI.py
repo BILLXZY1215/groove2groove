a1efe6eb67e46be48fc312e53acc38e68d3a83d4
Zeyu Xiong <scyzx2@gmail.com>
2021-11-13 15:47:22 +0800

    refctor image2MIDI

--- img2MIDI/image2MIDI.py
+++ img2MIDI/image2MIDI.py
@@ -15,38 +15,31 @@ print(img.shape)
 # Create a PrettyMIDI object
 cello_c_chord = pretty_midi.PrettyMIDI()
 # Create an Instrument instance for a cello instrument
+# TODO: Implement Instrument Name Category
 cello_program = pretty_midi.instrument_name_to_program('Cello')
 cello = pretty_midi.Instrument(program=cello_program)
 # Iterate over note names, which will be converted to note number later
+# Example: C Major
 # C D E F G A B C
 # 1 2 3 4 5 6 7 1
-for note_name in ['C5', 'E5', 'G5']:  # C (1,3,5)
-    note_number = pretty_midi.note_name_to_number(note_name)
-    print(note_number)
-    note = pretty_midi.Note(
-        velocity=100, pitch=note_number, start=0, end=2)
-    cello.notes.append(note)
-
-for note_name in ['A5', 'C5', 'E5']:  # Am (6,1,3)
-    note_number = pretty_midi.note_name_to_number(note_name)
-    print(note_number)
-    note = pretty_midi.Note(
-        velocity=100, pitch=note_number, start=2, end=4)
-    cello.notes.append(note)
-
-for note_name in ['F5', 'A5', 'C5']:  # F (4,6,1)
-    note_number = pretty_midi.note_name_to_number(note_name)
-    print(note_number)
-    note = pretty_midi.Note(
-        velocity=100, pitch=note_number, start=4, end=6)
-    cello.notes.append(note)
-
-for note_name in ['G5', 'B5', 'D5']:  # G (5,7,2)
-    note_number = pretty_midi.note_name_to_number(note_name)
-    print(note_number)
-    note = pretty_midi.Note(
-        velocity=100, pitch=note_number, start=6, end=8)
-    cello.notes.append(note)
+C = ['C5', 'E5', 'G5']  # 1 3 5
+Dm = ['D5', 'F5', 'A5']  # 2 4 6
+Em = ['E5', 'G5', 'B5']  # 3 5 7
+F = ['F5', 'A5', 'C5']  # 4 6 1
+G = ['G5', 'B5', 'D5']  # 5 7 2
+Am = ['A5', 'C5', 'E5']  # 6 1 3
+Bm = ['B5', 'D5', 'F5']  # 7 2 4
+# TODO: Implement chord database
+chord_progress = [C, Am, F, G]  # Sample: 1645
+i = 0
+for chord in chord_progress:
+    for note_name in chord:
+        note_number = pretty_midi.note_name_to_number(note_name)
+        print(note_number)
+        note = pretty_midi.Note(
+            velocity=100, pitch=note_number, start=0+1*i, end=1*(i+1))
+        cello.notes.append(note)
+    i = i + 1
 
 # Add the cello instrument to the PrettyMIDI object
 cello_c_chord.instruments.append(cello)
