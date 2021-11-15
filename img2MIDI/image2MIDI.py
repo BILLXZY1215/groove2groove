94a747bcc4daaa3ddcaf9db83bfc89c2765e84a3
Zeyu Xiong <scyzx2@gmail.com>
2021-11-15 15:11:55 +0800

    image2MIDI: findCloset func

--- img2MIDI/image2MIDI.py
+++ img2MIDI/image2MIDI.py
@@ -4,6 +4,53 @@ import numpy as np
 import pretty_midi
 import sys
 
+# Python program to find 3 elements such
+# that max(abs(A[i]-B[j]), abs(B[j]- C[k]),
+# abs(C[k]-A[i])) is minimized.
+import sys
+
+
+def findCloset(A, B, C):
+    p = len(A)
+    q = len(B)
+    r = len(C)
+    # Initialize min diff
+    diff = sys.maxsize
+    res_i = 0
+    res_j = 0
+    res_k = 0
+    # Traverse Array
+    i = 0
+    j = 0
+    k = 0
+    while(i < p and j < q and k < r):
+        # Find minimum and maximum of
+        # current three elements
+        minimum = min(A[i], min(B[j], C[k]))
+        maximum = max(A[i], max(B[j], C[k]))
+        # Update result if current diff is
+        # less than the min diff so far
+        if maximum-minimum < diff:
+            res_i = i
+            res_j = j
+            res_k = k
+            diff = maximum - minimum
+        # We can 't get less than 0 as
+        # values are absolute
+        if diff == 0:
+            break
+        # Increment index of array with
+        # smallest value
+        if A[i] == minimum:
+            i = i+1
+        elif B[j] == minimum:
+            j = j+1
+        else:
+            k = k+1
+        # Print result
+    print(A[res_i], ' ', B[res_j], ' ', C[res_k])
+    return [A[res_i],  B[res_j], C[res_k]]
+
 
 def image2MIDI(image_path, instrument):
 
